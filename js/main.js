257a00089c8532a06090defb98a83d37a0f832f6
Rafal <rafalkunickidev@gmail.com>
2022-07-28 22:16:29 +0100

    dreams section media queries and nav section

--- js/main.js
+++ js/main.js
@@ -10,5 +10,15 @@ const handleNav = () => {
       nav.classList.remove("nav-active");
     });
   });
+  handleNavItemsAnimation();
+};
+const handleNavItemsAnimation = () => {
+  let delayTime = 0;
+  allNavItems.forEach((item) => {
+    item.classList.toggle("-nav__items-animation");
+
+    item.style.animationDelay = "." + delayTime + "s";
+    delayTime++;
+  });
 };
 navBtn.addEventListener("click", handleNav);
